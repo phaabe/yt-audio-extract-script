e09684e9bd4b117d5d33fe5c000e6f8c6e42c6f2
Philipp Aaron Becker <philipp@local>
2023-11-13 19:05:12 +0100

    fix missing artist name by renaming all files in current dir

--- dl-audio.sh
+++ dl-audio.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# execute download and audio extraction
 yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 --output "%(artist)s - %(title)s.%(ext)s" --cookies-from-browser firefox "$1"
+
+# rename file where artist name is NA
+rename 's/^NA - //' "NA - "*.mp3
+
