b9f7021e81eb29db942d59ecf9499f8b351c7ada
Philipp Aaron Becker <philipp@local>
2023-12-01 16:17:38 -0600

    embed metadata and thumbnails

--- dl-audio.sh
+++ dl-audio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # execute download and audio extraction
-yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 --output "%(artist)s - %(title)s.%(ext)s" --cookies-from-browser firefox "$1"
+yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 --output "%(artist)s - %(title)s.%(ext)s" --add-metadata --embed-thumbnail --cookies-from-browser firefox "$1"
 
 # rename file where artist name is NA
 rename 's/^NA - //' "NA - "*.mp3
